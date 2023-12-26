3bf719da106f59befc6fbfe0f8ad7a696e450f54
Jean-François Grailet <jefgrailet@hotmail.com>
2023-12-26 16:54:39 +0100

    Nouveau lien du dépôt GitHub
    Mise à jour du lien vers le dépôt GitHub (suite au changement de nom, à savoir JeuxRedige au lieu de JeuxRedigeBE).

--- src/view/Footer.inc.php
+++ src/view/Footer.inc.php
@@ -11,7 +11,7 @@ echo WebpageHandler::$container['end'];
          <p>
             <a href="#main" class="iconLink"><i class="icon-general_up" style="font-size: 24px;" title="Revenir en haut de page"></i></a> 
             <a href="<?php echo $webRootPath; ?>" class="iconLink"><i class="icon-general_home" style="font-size: 24px;" title="Revenir à l'accueil"></i></a> 
-            <a href="https://github.com/JefGrailet/JeuxRedigeBE" target="blank" class="iconLink"><i class="icon-ext_github" style="font-size: 24px;" title="Code source sur GitHub"></i></a> 
+            <a href="https://github.com/JefGrailet/JeuxRedige" target="blank" class="iconLink"><i class="icon-ext_github" style="font-size: 24px;" title="Code source sur GitHub"></i></a> 
             <a href="https://www.youtube.com/@jeuxredige" target="blank" class="iconLink"><i class="icon-ext_youtube_logo" style="font-size: 24px;" title="Notre chaîne YouTube"></i></a>
             <a href="https://twitter.com/JeuxRedigeBE" target="blank" class="iconLink"><i class="icon-social_x" style="font-size: 24px;" title="Suivez-nous sur X (Twitter)"></i></a>
          </p>

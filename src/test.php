19fb0a0c31e1ef47f3ea1cf1590b820ff02ce878
PC-Dan <jeanlouis.danielo@sfr.fr>
2025-09-03 18:06:06 +0200

    Suppression du fichier de test

--- src/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-  require_once('vendor/autoload.php');
-
-  $loader = new \Twig\Loader\FilesystemLoader('./view');
-  $twig = new \Twig\Environment($loader);
-
-  echo $twig->render('demo.twig', ['name' => 'Fabien']);

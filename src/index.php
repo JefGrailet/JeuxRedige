94853ff613426d5333a7efe36e0a76ff9095678f
PC-Dan <jeanlouis.danielo@sfr.fr>
2025-09-10 20:19:36 +0200

    wip - Ajout de twig sur la page d'accueil

--- src/index.php
+++ src/index.php
@@ -1,8 +1,8 @@
 <?php
 
 /**
-* Home page.
-*/
+ * Home page.
+ */
 
 require './libraries/Header.lib.php';
 require './model/Article.class.php';
@@ -10,103 +10,109 @@ require './model/Topic.class.php';
 require './view/intermediate/ArticleThumbnail.ir.php';
 require './view/intermediate/TopicThumbnail.ir.php';
 
+require_once './vendor/autoload.php';
+
 WebpageHandler::redirectionAtLoggingIn();
 
 // Webpage settings
 WebpageHandler::addCSS('pool');
 WebpageHandler::noContainer();
 
+$loader = new \Twig\Loader\FilesystemLoader('./views');
+
+$twig = new \Twig\Environment($loader, [
+   'debug' => true,
+]);
+$twig->addExtension(new \Twig\Extension\DebugExtension());
+
+$function = new \Twig\TwigFunction('cancel', function ($msg = "Une erreur est survenue") {
+    throw new \Twig\Error\Error("Process cancelled with msg: $msg");
+});
+$twig->addFunction($function);
+
+$twig->addGlobal("webRoot", PathHandler::HTTP_PATH());
+$twig->addGlobal("configJS", "");
+$twig->addGlobal("extJS", PathHandler::JS_EXTENSION());
+$twig->addGlobal("JSFiles", "");
+$twig->addGlobal("autoJS", "");
+$twig->addGlobal("articles_categories", [
+   "review" => [
+      "name" => "Critique",
+   ],
+   "preview" => [
+      "name" => "Aperçu",
+   ],
+   "opinion" => [
+      "name" => "Humeur",
+   ],
+   "chronicle" => [
+      "name" => "Chronique",
+   ],
+   "guide" => [
+      "name" => "Guide",
+   ],
+   "misc" => [
+      "name" => "Hors-Jeu",
+   ],
+]);
+$twig->addGlobal("meta", [
+   "title" => "JeuxRédige",
+   "description" => "Critiques et chroniques sur le jeu vidéo par des passionnés",
+   "image" => "https://" . $_SERVER["HTTP_HOST"] . "/default_meta_logo.jpg",
+   "url" => "https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"],
+   "full_title" => "",
+]);
+$twig->addGlobal("dialogs", "");
+$twig->addGlobal("selectedLogo", "default");
+$twig->addGlobal("userSide", "default");
+$twig->addGlobal("is_user_logged", LoggedUser::isLoggedIn());
+if (LoggedUser::isLoggedIn()) {
+   $twig->addGlobal("userInfos", array(
+      'avatar' => PathHandler::getAvatarMedium(LoggedUser::$data['used_pseudo']),
+      'pseudo' => LoggedUser::$data['pseudo'],
+   ));
+}
+$twig->addGlobal("renderTime", number_format(microtime(true) - $_SERVER['REQUEST_TIME_FLOAT'], 5, '.', ""));
+
 // Gets the last featured articles
 $articles = null;
-try
-{
+try {
    $articles = Article::getFeaturedArticles(8);
-}
-catch(Exception $e)
-{
+} catch (Exception $e) {
    $errorTplInput = array('error' => 'dbError');
    $tpl = TemplateEngine::parse('view/content/Index.fail.ctpl', $errorTplInput);
    WebpageHandler::wrap($tpl, 'Impossible d\'atteindre le contenu');
 }
 
-if($articles == NULL)
-{
+if ($articles == NULL) {
    $errorTplInput = array('error' => 'noContent');
    $tpl = TemplateEngine::parse('view/content/Index.fail.ctpl', $errorTplInput);
    WebpageHandler::wrap($tpl, 'Impossible d\'atteindre le contenu');
 }
 
-/* From this point, all the content has been extracted from the DB. All what is left to do is
-* to render both article and topic thumbnails. */
 
-$fullInput = array();
-for($i = 0; $i < count($articles); $i++)
-{
-   $intermediate = ArticleThumbnailIR::process($articles[$i]);
-   array_push($fullInput, $intermediate);
-}
+$NB_MAX_ARTICLES_HIGHLIGHTED = 2;
 
-// Sorts articles depending on whether they have a highlight or not
-$fullInputLarge = array();
-$fullInputSmall = array();
-$maxHighlight = 2;
-for($i = 0; $i < count($fullInput); $i++)
-{
-   $highlight = Article::getHighlightStatic($fullInput[$i]['ID']);
-   if(strlen($highlight) > 0 && count($fullInputLarge) < $maxHighlight)
-   {
-      $fullInput[$i]['highlight'] = $highlight;
-      array_push($fullInputLarge, $fullInput[$i]);
-   }
-   else
-      array_push($fullInputSmall, $fullInput[$i]);
-}
+$listArticlesComputed = array_map(function ($article, $idx) use ($NB_MAX_ARTICLES_HIGHLIGHTED) {
+   return array(
+      ...$article,
+      "is_highlighted" => $idx < $NB_MAX_ARTICLES_HIGHLIGHTED,
+      "link" => ArticleThumbnailIR::getLink($article),
+      "date_time" => ArticleThumbnailIR::getDateTime($article),
+      "thumbnail" => ArticleThumbnailIR::getThumbnail($article),
+   );
+}, $articles, array_keys($articles));
 
-// Rendered article thumbnails
-$articleThumbnails = '';
-if(count($fullInputLarge) > 0)
-{
-   $fullOutputLarge = TemplateEngine::parseMultiple('view/content/ArticleHighlight.ctpl', $fullInputLarge);
-   if(TemplateEngine::hasFailed($fullOutputLarge))
-   {
-      $errorTplInput = array('error' => 'wrongTemplating');
-      $tpl = TemplateEngine::parse('view/content/Index.fail.ctpl', $errorTplInput);
-      WebpageHandler::wrap($tpl, 'Impossible d\'atteindre le contenu');
-   }
+$listCSSFiles = ["pool"];
 
-   for($i = 0; $i < count($fullOutputLarge); $i++)
-      $articleThumbnails .= $fullOutputLarge[$i];
-}
-
-if(count($fullInputSmall) > 0)
-{
-   $fullOutputSmall = TemplateEngine::parseMultiple('view/content/ArticleThumbnail.ctpl', $fullInputSmall);
-   if(TemplateEngine::hasFailed($fullOutputSmall))
-   {
-      $errorTplInput = array('error' => 'wrongTemplating');
-      $tpl = TemplateEngine::parse('view/content/Index.fail.ctpl', $errorTplInput);
-      WebpageHandler::wrap($tpl, 'Impossible d\'atteindre le contenu');
-   }
-
-   for($i = 0; $i < count($fullOutputSmall); $i++)
-      $articleThumbnails .= $fullOutputSmall[$i];
-}
-
-// Meta-tags
-WebpageHandler::$miscParams['meta_title'] = "JeuxRédige";
-WebpageHandler::$miscParams['meta_description'] = "Critiques et chroniques sur le jeu vidéo par des passionnés";
-WebpageHandler::$miscParams['meta_image'] = "https://".$_SERVER['HTTP_HOST']."/default_meta_logo.jpg";
-WebpageHandler::$miscParams['meta_url'] = "https://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
-
-// Webpage keywords
-WebpageHandler::$miscParams['meta_keywords'] = 'JeuxRédige, Jeux Rédige, jeux rédige, jeux redige, jeux vidéo, jeu vidéo';
-
-// Final HTML code (with page configuration)
-$finalTplInput['articles'] = $articleThumbnails;
-
-$content = TemplateEngine::parse('view/content/Index.ctpl', $finalTplInput);
-
-// Displays the produced page
-WebpageHandler::wrap($content, '');
-
-?>
+echo $twig->render("index.html.twig", [
+   "list_articles" => $listArticlesComputed,
+   "list_css_files" => ["pool"],
+   "meta" => [
+      "title" => "JeuxRédige",
+      "description" => "Critiques et chroniques sur le jeu vidéo par des passionnés",
+      "image" => "https://" . $_SERVER["HTTP_HOST"] . "/default_meta_logo.jpg",
+      "url" => "https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"],
+      "full_title" => "",
+   ]
+]);

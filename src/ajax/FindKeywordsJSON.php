eaa945d539a6e224437fe635746a950ab86062f4
PC-Dan <jeanlouis.danielo@sfr.fr>
2025-09-11 23:27:22 +0200

    Ajout de select2

---
+++ src/ajax/FindKeywordsJSON.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+* Script to search for tags/keywords containing a given input string the user wrote in some input
+* text field. The script receives the "needle" as a $_POST value and returns HTML code consisting
+* of either keywords containing the needle (with a new line between each of them) either a single
+* line telling this keyword does not exist yet in the database.
+*/
+
+
+require '../libraries/Header.lib.php';
+require '../model/Tag.class.php';
+
+
+header('Content-Type: application/json; charset=utf-8');
+
+
+if(isset($_GET['keyword']) && !empty($_GET['keyword']))
+{
+   $needle = Utils::secure($_GET['keyword']);
+   $needle = str_replace('|', '', $needle); // Security, same for next line
+   $needle = str_replace('"', '', $needle);
+
+   if($needle === '')
+   {
+      echo json_encode([]);
+
+      return;
+   }
+
+   try
+   {
+      $results = Tag::findTags($needle);
+      $nbResults = count($results);
+      sort($results);
+      echo json_encode($results);
+   }
+   catch(Exception $e)
+   {
+      echo json_encode([]);
+   }
+}
+
+?>
---
+++ src/javascript/search_articles.js
@@ -0,0 +1,47 @@
+function formatState(state) {
+   if (state.loading) {
+      return "Chargement...";
+   }
+
+   const $tpl = $(`
+      <div class="keyword-result">
+         ${state}
+      </div>
+   `);
+
+   return $tpl;
+}
+
+$("[data-dropdown-keywords]").select2({
+   placeholder: "Entrez un mot-clef",
+   minimumInputLength: 2,
+   minimumResultsForSearch: 2,
+   language: "fr",
+   cache: true,
+   ajax: {
+      delay: 250,
+      url: "./ajax/FindKeywordsJSON.php",
+      dataType: "json",
+      data: function (params) {
+         const query = {
+            keyword: params.term,
+         };
+
+         return query;
+      },
+      processResults: function (data) {
+         return {
+            results: data.map((item) => ({ text: item, id: item })),
+         };
+      },
+   },
+   // templateResult: formatState,
+});
+
+const urlParams = new URLSearchParams(window.location.search);
+const listKeywords = urlParams.getAll('keyword[]');
+
+listKeywords.forEach((keyword) => {
+   const option = new Option(keyword, keyword, true, true);
+   $("[data-dropdown-keywords]").append(option).trigger('change');
+});

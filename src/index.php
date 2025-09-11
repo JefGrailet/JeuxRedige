883a0dea532ba853d73bd3980daf495cb30b4d56
PC-Dan <jeanlouis.danielo@sfr.fr>
2025-09-11 13:06:58 +0200

    Création d'un fichier dédié pour la configuration de twig

--- src/index.php
+++ src/index.php
@@ -1,74 +1,16 @@
 <?php
-
 /**
  * Home page.
  */
 
 require './libraries/Header.lib.php';
 require './model/Article.class.php';
-require './model/Topic.class.php';
 require './view/intermediate/ArticleThumbnail.ir.php';
-require './view/intermediate/TopicThumbnail.ir.php';
 
 require_once './vendor/autoload.php';
+require_once './libraries/core/Twig.config.php';
 
 WebpageHandler::redirectionAtLoggingIn();
-
-
-$loader = new \Twig\Loader\FilesystemLoader('./views');
-
-$twig = new \Twig\Environment($loader, [
-   'debug' => true,
-]);
-$twig->addExtension(new \Twig\Extension\DebugExtension());
-
-$twig->addGlobal("webRoot", PathHandler::HTTP_PATH());
-$twig->addGlobal("configJS", "");
-$twig->addGlobal("extJS", PathHandler::JS_EXTENSION());
-$twig->addGlobal("JSFiles", "");
-$twig->addGlobal("autoJS", "");
-$twig->addGlobal("articles_categories", [
-   "review" => [
-      "name" => "Critique",
-   ],
-   "preview" => [
-      "name" => "Aperçu",
-   ],
-   "opinion" => [
-      "name" => "Humeur",
-   ],
-   "chronicle" => [
-      "name" => "Chronique",
-   ],
-   "guide" => [
-      "name" => "Guide",
-   ],
-   "misc" => [
-      "name" => "Hors-Jeu",
-   ],
-]);
-$twig->addGlobal("meta", [
-   "title" => "JeuxRédige",
-   "description" => "Critiques et chroniques sur le jeu vidéo par des passionnés",
-   "image" => "https://" . $_SERVER["HTTP_HOST"] . "/default_meta_logo.jpg",
-   "url" => "https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"],
-   "full_title" => "",
-]);
-$twig->addGlobal("dialogs", "");
-$twig->addGlobal("selectedLogo", "default");
-$twig->addGlobal("userSide", "default");
-$twig->addGlobal("is_user_logged", LoggedUser::isLoggedIn());
-if (LoggedUser::isLoggedIn()) {
-   $twig->addGlobal("userInfos", array(
-      "avatar" => PathHandler::getAvatarMedium(LoggedUser::$data['used_pseudo']),
-      "pseudo" => LoggedUser::$data['pseudo'],
-      "is_admin" => LoggedUser::$data['function_pseudo'] !== NULL && strlen(LoggedUser::$data['function_pseudo']) > 0 && LoggedUser::$data['function_name'] !== 'alumnus',
-      "is_using_admin_account" => LoggedUser::$data['function_pseudo'] === LoggedUser::$data['used_pseudo'],
-      "alt_pseudo" => LoggedUser::$data['function_pseudo'],
-      "test" => LoggedUser::$data['function_name'] === 'administrator',
-   ));
-}
-$twig->addGlobal("renderTime", number_format(microtime(true) - $_SERVER['REQUEST_TIME_FLOAT'], 5, '.', ""));
 
 // Gets the last featured articles
 $articles = null;
@@ -104,7 +46,9 @@ $listCSSFiles = ["pool"];
 echo $twig->render("index.html.twig", [
    "list_articles" => $listArticlesComputed,
    "list_css_files" => ["pool"],
+   "selectedLogo" => "default",
    "meta" => [
+      ...$twig->getGlobals()["meta"],
       "title" => "JeuxRédige",
       "description" => "Critiques et chroniques sur le jeu vidéo par des passionnés",
       "image" => "https://" . $_SERVER["HTTP_HOST"] . "/default_meta_logo.jpg",
---
+++ src/libraries/core/Twig.config.php
@@ -0,0 +1,59 @@
+<?php
+
+$loader = new \Twig\Loader\FilesystemLoader('./views');
+
+$twig = new \Twig\Environment($loader, [
+   'debug' => true,
+]);
+$twig->addExtension(new \Twig\Extension\DebugExtension());
+
+$twig->addGlobal("webRoot", PathHandler::HTTP_PATH());
+$twig->addGlobal("configJS", "");
+$twig->addGlobal("extJS", PathHandler::JS_EXTENSION());
+$twig->addGlobal("JSFiles", "");
+$twig->addGlobal("autoJS", "");
+$twig->addGlobal("articles_categories", [
+   "review" => [
+      "name" => "Critique",
+   ],
+   "preview" => [
+      "name" => "Aperçu",
+   ],
+   "opinion" => [
+      "name" => "Humeur",
+   ],
+   "chronicle" => [
+      "name" => "Chronique",
+   ],
+   "guide" => [
+      "name" => "Guide",
+   ],
+   "misc" => [
+      "name" => "Hors-Jeu",
+   ],
+]);
+$twig->addGlobal("meta", [
+   "title" => "JeuxRédige",
+   "description" => "Critiques et chroniques sur le jeu vidéo par des passionnés",
+   "image" => "https://" . $_SERVER["HTTP_HOST"] . "/default_meta_logo.jpg",
+   "url" => "https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"],
+   "full_title" => "",
+]);
+$twig->addGlobal("dialogs", "");
+$twig->addGlobal("page_title", "Site de critiques de jeux vidéo");
+$twig->addGlobal("selectedLogo", "default");
+$twig->addGlobal("userSide", "default");
+$twig->addGlobal("is_user_logged", LoggedUser::isLoggedIn());
+if (LoggedUser::isLoggedIn()) {
+   $twig->addGlobal("userInfos", array(
+      "avatar" => PathHandler::getAvatarMedium(LoggedUser::$data['used_pseudo']),
+      "pseudo" => LoggedUser::$data['pseudo'],
+      "is_admin" => LoggedUser::$data['function_pseudo'] !== NULL && strlen(LoggedUser::$data['function_pseudo']) > 0 && LoggedUser::$data['function_name'] !== 'alumnus',
+      "is_using_admin_account" => LoggedUser::$data['function_pseudo'] === LoggedUser::$data['used_pseudo'],
+      "alt_pseudo" => LoggedUser::$data['function_pseudo'],
+      "test" => LoggedUser::$data['function_name'] === 'administrator',
+   ));
+}
+$twig->addGlobal("renderTime", number_format(microtime(true) - $_SERVER['REQUEST_TIME_FLOAT'], 5, '.', ""));
+parse_str($queryString["QUERY_STRING"], $queryString);
+$twig->addGlobal("query_string", $queryString);

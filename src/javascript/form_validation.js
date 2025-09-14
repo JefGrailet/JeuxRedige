e349d2142da75685cead701a042615b79deb1df9
PC-Dan <jeanlouis.danielo@sfr.fr>
2025-09-14 13:02:52 +0200

    Ajout validation par javascript pour les formulaires

--- src/javascript/form_validation.js
+++ src/javascript/form_validation.js
@@ -2,24 +2,23 @@ const Joi = window.joi;
 
 const formValidation = (e) => {
    e.preventDefault();
-
+   const form = e.currentTarget;
    const schemaName = e.currentTarget.dataset.formSchemaValidation;
    let schema = new Function(document.querySelector(`[data-form-schema=${schemaName}]`)?.textContent)();
 
    if (schema) {
-      const formData = new FormData(e.currentTarget);
-      schema = schema.options({ allowUnknown: true });
+      const formData = new FormData(form);
+      schema = schema.options({ allowUnknown: true, abortEarly: false });
       const validator = schema.validate(Object.fromEntries(formData));
       if (validator.error) {
-         const bannerError = e.currentTarget.querySelector(`[data-form-error=${schemaName}]`);
+         const bannerError = form.querySelector(`[data-form-error=${schemaName}]`);
          if (bannerError) {
             bannerError.textContent = validator.error.message
          }
+         return;
       }
-      console.log()
-   } else {
-      alert("okkk");
    }
+   form.submit();
 }
 
 const listForms = document.querySelectorAll("[data-form-schema-validation]");

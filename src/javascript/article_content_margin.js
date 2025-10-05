23058e66a1e0bceaf7bdb05730b68e31d20d816d
PC-Dan <jeanlouis.danielo@sfr.fr>
2025-10-05 12:48:57 +0200

    Affichage dynamique de l'image header d'un article

---
+++ src/javascript/article_content_margin.js
@@ -0,0 +1,16 @@
+const articleContent = document.querySelector("[data-article-content]");
+const articleHeader = document.querySelector("[data-article-header]");
+
+articleContent.style.marginTop = `${articleHeader.offsetHeight}px`;
+
+window.addEventListener("resize", () => {
+   if (window.scrollY !== 0) {
+      articleContent.style.marginTop = `${articleHeader.offsetHeight}px`;
+   }
+});
+
+screen.orientation.addEventListener("change", () => {
+   if (window.scrollY !== 0) {
+      articleContent.style.marginTop = `${articleHeader.offsetHeight}px`;
+   }
+});

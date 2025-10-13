f1917437c6588fd4f834aaf3a37e0bd715e5c648
PC-Dan <jeanlouis.danielo@sfr.fr>
2025-10-13 18:09:47 +0200

    Amélioration des performances du calcul de la position d'un article

--- src/javascript/article_content_margin.js
+++ src/javascript/article_content_margin.js
@@ -3,14 +3,22 @@ const articleHeader = document.querySelector("[data-article-header]");
 
 articleContent.style.marginTop = `${articleHeader.offsetHeight}px`;
 
-window.addEventListener("resize", () => {
-   if (window.scrollY === 0) {
-      articleContent.style.marginTop = `${articleHeader.offsetHeight}px`;
-   }
-});
+// window.addEventListener("resize", () => {
+//    if (window.scrollY === 0) {
+//       articleContent.style.marginTop = `${articleHeader.offsetHeight}px`;
+//    }
+// });
 
 screen.orientation.addEventListener("change", () => {
    if (window.scrollY === 0) {
       articleContent.style.marginTop = `${articleHeader.offsetHeight}px`;
    }
 });
+
+const observer = new IntersectionObserver((entries) => {
+   if (entries[0].isIntersecting) {
+      articleContent.style.marginTop = `${articleHeader.offsetHeight}px`;
+   }
+});
+
+observer.observe(document.querySelector("#pixel-to-watch"));

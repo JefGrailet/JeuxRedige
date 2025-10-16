a78d9758e00254f35160c5191981ede614eb2fa0
PC-Dan <jeanlouis.danielo@sfr.fr>
2025-10-16 17:58:35 +0200

    Refactoring de la gestion des couleurs par article

--- src/Article.php
+++ src/Article.php
@@ -213,7 +213,7 @@ if(!empty($_GET['id_article']) && preg_match('#^([0-9]+)$#', $_GET['id_article']
       "list_css_files" => ["article", 'charter_'.$article->get('type'), "badge"],
       "list_js_files" => ["article", "article_content_margin"],
       "selectedLogo" => $article->get('type'),
-      "articleColor" => $twig->getGlobals()["list_categories"][$article->get('type')]["color"],
+      "currentCategory" => $article->get('type'),
       "meta" => [
          ...$twig->getGlobals()["meta"],
          "title" => $title,

e0f9c17efd013aafe31800b668b57e6fbd4fbf31
PC-Dan <jeanlouis.danielo@sfr.fr>
2025-10-19 19:51:15 +0200

    Renommage fichier

---
+++ src/javascript/modals_page.js
@@ -0,0 +1,40 @@
+const deletePageModal = document.getElementById("delete-page");
+
+deletePageModal?.addEventListener("toggle", (e) => {
+   if (e.newState === "open") {
+      const pageData = JSON.parse(e.source.dataset.pageData);
+
+      e.target.querySelector("[name=id_segment]").value = pageData.id;
+      e.target.querySelector(".title span").textContent = pageData.title || "Sommaire";
+   }
+});
+
+const integrateMediaPageModal = document.getElementById("integrate-media");
+
+integrateMediaPageModal?.addEventListener("toggle", (e) => {
+   if (e.newState === "open") {
+      const mediaData = JSON.parse(e.source.dataset.mediaData);
+
+      e.target.querySelector("img").src = mediaData.mini.src;
+      e.target.querySelector("[name=url_img]").value = mediaData.full.srcRelative;
+      e.target.querySelector(".title span").textContent = pageData.title || "Sommaire";
+   } else {
+      const imgSize = e.target.querySelector("[name=format_img]").value;
+      const imgPosition = e.target.querySelector("[name=floating_img]").value;
+      const imgPath = e.target.querySelector("[name=url_img]").value;
+      const imgNote = e.target.querySelector("[name=comment_img]").value;
+   }
+});
+
+const previewMediaModal = document.getElementById("preview-media");
+
+previewMediaModal?.addEventListener("toggle", (e) => {
+   if (e.newState === "open") {
+      const mediaData = JSON.parse(e.source.dataset.mediaData);
+      console.log(mediaData)
+
+      e.target.querySelector(".content img").src = mediaData.full.src;
+      e.target.querySelector("[data-date]").textContent = mediaData.uploadDate;
+      e.target.querySelector("a").href = mediaData.full.src;
+   }
+});
